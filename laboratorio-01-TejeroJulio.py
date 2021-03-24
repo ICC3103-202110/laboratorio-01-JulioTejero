7f0d85fc5929708a8a9240679a7840e05b8d679b
JulioTejero <jtejero@miuandes.cl>
2021-03-23 21:23:00 -0300

    Update laboratorio-01-TejeroJulio.py

--- laboratorio-01-TejeroJulio.py
+++ laboratorio-01-TejeroJulio.py
@@ -14,30 +14,75 @@ while n <= numero_cartas:
 
 random.shuffle(lista_tablero)
 
-def tablero(r):
-    tablero_oculto = []
-    tablero_numeros = []
-    n = int((2*r)**(1/2))
+tablero_oculto = []
+tablero_numeros = []
+n = int((2*numero_cartas)**(1/2))
+z = 0
+
+if (n**2) == 2*numero_cartas:
+
+    for x in range(0,n):
+
+        tablero_oculto_add = []
+
+        for i in range(0,n):
+
+            tablero_oculto_add.append("*")
+
+        tablero_oculto.append(tablero_oculto_add)
+
+    for a in range(0,n):
+
+        tablero_numeros_add = []
+
+        for b in range(0,n):
+
+            tablero_numeros_add.append(lista_tablero[z])
+            z += 1
+
+        tablero_numeros.append(tablero_numeros_add)
+            
+else:
+
+    numero_filas = int((2*numero_cartas)**(1/2))
+    numero_columnas = numero_filas + 1  
+    
+    for i in range(0,numero_filas):
+
+        tablero_oculto_add = []
+
+        for x in range(0, numero_columnas):
+
+            if z < len(lista_tablero):
+
+                tablero_oculto_add.append("*")
+                z += 1
+
+            else:
+
+                tablero_oculto_add.append ("")
+                z += 1
+    
     z = 0
 
-    if (n**2) == 2*numero_cartas:
-        for x in range(0,n):
-            tablero_oculto_add = []
-            for i in range(0,n):
-                tablero_oculto_add.append("*")
-            tablero_oculto.append(tablero_oculto_add)
+    for i in range(0,numero_filas):
 
-        for a in range(0,n):
-            tablero_numeros_add = []
-            for b in range(0,n):
-                print(z)
+        tablero_numeros_add = []
+
+        for x in range(0, numero_columnas):
+
+            if z < len(lista_tablero):
+
                 tablero_numeros_add.append(lista_tablero[z])
+
                 z += 1
-            tablero_numeros.append(tablero_numeros_add)
-                
 
-    print(tablero_oculto)
-    print(tablero_numeros)
+            else:
 
-tablero(numero_cartas)
+                tablero_numeros_add.append ("")
+                z += 1
 
+        
+        tablero_numeros.append(tablero_numeros_add)
+
+print(tablero_numeros)
